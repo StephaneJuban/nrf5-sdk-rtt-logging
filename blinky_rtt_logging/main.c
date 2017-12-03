3fcd910ec0750e9c084e284f8225ee56d7edf3b7
Stéphane Juban <stephane@ekico.fr>
2017-12-03 12:53:45 +0100

    Initial commit

---
+++ blinky_rtt_logging/main.c
@@ -0,0 +1,45 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "nrf_delay.h"
+#include "boards.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+/* OPTIONAL - Add a prefix to your log message */
+/* #define NRF_LOG_MODULE_NAME module_name
+NRF_LOG_MODULE_REGISTER(); */
+
+
+/**
+ * @brief Function for application main entry.
+ */
+int main(void)
+{
+    /* Initializing Log */
+    ret_code_t err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+
+    /* Configure board. */
+    bsp_board_leds_init();
+
+    /* Toggle LEDs. */
+    while (true)
+    {
+        for (int i = 0; i < LEDS_NUMBER; i++)
+        {
+            bsp_board_led_invert(i);
+
+            /* Create a log message */
+            NRF_LOG_INFO("BLINKING LED n°%d", i);
+            /* Process the log */
+            NRF_LOG_PROCESS();
+
+            nrf_delay_ms(500);
+        }
+    }
+}
+
+
